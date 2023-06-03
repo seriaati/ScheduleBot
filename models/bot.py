410f4d2d751dd5540e14b70b6862fc830c81e1b2
seriaati <seria.ati@gmail.com>
2023-06-03 15:10:11 +0800

    feat: add translator

--- models/bot.py
+++ models/bot.py
@@ -1,5 +1,11 @@
+from typing import Tuple
+
+from discord import app_commands
+from discord.app_commands.translator import TranslationContextTypes, locale_str
+from discord.enums import Locale
 from discord.ext import commands
 
+from i18n.translator import translator
 from models.db.database import DataBase
 
 
@@ -7,7 +13,34 @@ class Bot(commands.Bot):
     """
     A subclass of commands.Bot.
 
-    This class adds a db attribute to the bot which is an instance of the DataBase class.
+    Attributes:
+        db (DataBase): The database.
     """
 
     db: DataBase
+    owner_ids: Tuple[int, ...]
+
+
+class BotTranslator(app_commands.Translator):
+    """
+    The translator for the bot.
+    """
+
+    async def translate(
+        self, string: locale_str, locale: Locale, _: TranslationContextTypes
+    ) -> str:
+        """
+        Translate a string to the specified language.
+
+        Args:
+            string (str): The string to translate.
+            locale (Locale): The language to translate to.
+            _ (TranslationContextTypes): The context of the string.
+
+        Returns:
+            str: The translated string.
+        """
+        try:
+            return translator.translate(locale.value, string.extras["context"])
+        except KeyError:
+            return None
